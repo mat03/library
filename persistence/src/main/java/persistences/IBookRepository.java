9ee6073b6244d20c4be6eb84fd34166b9ca9fcc2
Mateusz Fiolek <fiolek.m@gmail.com>
2018-07-21 15:42:32 +0200

    add book add

--- persistence/src/main/java/persistences/IBookRepository.java
+++ persistence/src/main/java/persistences/IBookRepository.java
@@ -2,10 +2,11 @@ package persistences;
 
 import models.Book;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public interface IBookRepository {
-    void add(Book book);
+    void add(Book book) throws IOException;
     void remove(Long id);
     void update(Book book);
     Book get(Long id);
